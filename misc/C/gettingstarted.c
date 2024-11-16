7304c9749c495e09dbb6fb1d6245f4c810bfbfc1
Jai Sharma <jaisharma687@outlook.com>
2024-11-16 11:09:04 +0530

    modifications

--- misc/C/gettingstarted.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> // This is a preprocessor directive that tells the compiler to include the standard input/output library
-
-int main() // This is the main function where the program execution begins
-{
-    printf("Hello World"); // This is a function call that displays the message inside the quotation marks
-    return 0; // This is the exit status of the program
-}
